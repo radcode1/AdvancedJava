ea88038e2815c8316c8fff3ed0f0e69e46299c56
radcode1 <radhika.tata7@gmail.com>
2024-09-18 10:34:55 -0700

    Added the project with WelcomeServlet

---
+++ WelcomeServletUsingAnnotations/src/main/java/com/learn/WelcomeServlet.java
@@ -0,0 +1,30 @@
+package com.learn;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/annotation")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String var1 = request.getParameter("var1");
+		
+		// Second Parameter
+		String var2 = request.getParameter("var2");
+		
+		response.getWriter().append("Welcome " + var1 +  " "+ var2).close();
+	}
+
+}
